edd6cdb8352dc13a387049702a1078013d292c9a
gokde <gokdenizdincer61@gmail.com>
2024-05-16 16:23:06 +0300

    update 1.3 (login-register)

--- app/src/main/java/com/example/masterphotos/ProfileFragment.java
+++ app/src/main/java/com/example/masterphotos/ProfileFragment.java
@@ -1,21 +1,97 @@
 package com.example.masterphotos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
     public ProfileFragment() {
 
     }
+
+    TextInputEditText edittext_email, edittext_password;
+
+    Button btn_login;
+
+    FirebaseAuth mAuth;
+
+    TextView registernow;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        edittext_email = view.findViewById(R.id.lg_email);
+        edittext_password = view.findViewById(R.id.lg_password);
+        btn_login = view.findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+        registernow = view.findViewById(R.id.txt_registernow);
+
+        registernow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                RegisterFragment registerFragment = new RegisterFragment();
+
+                transaction.replace(R.id.fragment_container, registerFragment);
+
+                transaction.commit();
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = edittext_email.getText().toString();
+                password = edittext_password.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(getActivity(), "Please fill all the blanks.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    String userEmail = edittext_email.getText().toString();
+
+                                    Toast.makeText(getActivity(), "Login successful", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    Toast.makeText(getActivity(), "Login failed.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
         return view;
     }
+    }
 
-}
+
+
--- app/src/main/java/com/example/masterphotos/RegisterFragment.java
+++ app/src/main/java/com/example/masterphotos/RegisterFragment.java
@@ -1,4 +1,89 @@
 package com.example.masterphotos;
 
-public class RegisterFragment {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterFragment extends Fragment {
+
+
+    TextInputEditText edittext_email, edittext_password;
+
+    Button btn_register;
+
+    FirebaseAuth mAuth;
+
+    TextView loginnow;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        edittext_email = view.findViewById(R.id.rg_email);
+        edittext_password = view.findViewById(R.id.rg_password);
+        btn_register = view.findViewById(R.id.btn_register);
+        loginnow = view.findViewById(R.id.txt_loginnow);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                ProfileFragment profileFragment = new ProfileFragment();
+
+                transaction.replace(R.id.fragment_container, profileFragment);
+
+                transaction.commit();
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = edittext_email.getText().toString();
+                password = edittext_password.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(getActivity(), "Please fill all the blanks.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(), "Register successful.", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(getActivity(), "Register failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        return view;
+    }
+
 }
